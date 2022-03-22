8e4a1b46ab6a015761213103a704000ab34e7a57
edgxxar <janpetto@web.de>
2022-03-22 12:45:29 +0100

    consistent use of base_url for API calls. Maybe this should be moved to a utility class later, it's the same for all modules so far.

--- plugins/modules/discovery.py
+++ plugins/modules/discovery.py
@@ -116,12 +116,16 @@ def run_module():
         'Authorization': 'Bearer ' + automation_user + ' ' + automation_secret
     }
 
-    api_endpoint = '/objects/host/' + host_name + '/actions/discover_services/invoke'
     params = {
         'mode': state,
     }
-    url = server_url + site + "/check_mk/api/1.0" + api_endpoint
 
+    base_url = "%s/%s/check_mk/api/1.0" % (
+        server_url,
+        site,
+    )
+    api_endpoint = '/objects/host/' + host_name + '/actions/discover_services/invoke'
+    url = base_url + api_endpoint
     response, info = fetch_url(module, url, module.jsonify(params), headers=headers, method='POST')
     http_code = info['status']
 
