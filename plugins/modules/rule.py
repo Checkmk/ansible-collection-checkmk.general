8a9490cf640da3d924867c3995f5aa51d13c8875
diademiemi <emilia@diademiemi.me>
2022-08-30 14:26:27 +0200

    Fix whitespace

--- plugins/modules/rule.py
+++ plugins/modules/rule.py
@@ -18,7 +18,7 @@ short_description: Manage rules in Checkmk.
 version_added: "0.8.0"
 
 description:
-- Manage rules within Checkmk. Importing rules from the output of the Checkmk API. 
+- Manage rules within Checkmk. Importing rules from the output of the Checkmk API.
 - Make sure these were exported with Checkmk 2.1.0p10 or above. See https://checkmk.com/werk/14670 for more information.
 
 extends_documentation_fragment: [tribe29.checkmk.common]
