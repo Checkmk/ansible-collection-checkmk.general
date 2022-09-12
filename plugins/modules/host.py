03b654a96a2966f0f9b8d0794670864c163c7686
Lars Getwan <lars.getwan@tribe29.com>
2022-09-12 11:44:44 +0200

    Fix #99 - Modifying host with same folder

--- plugins/modules/host.py
+++ plugins/modules/host.py
@@ -137,7 +137,7 @@ def get_current_host_state(module, base_url, headers):
         etag = info.get("etag", "")
         extensions = body.get("extensions", {})
         current_explicit_attributes = extensions.get("attributes", {})
-        current_folder = "/%s" % extensions.get("folder", "")
+        current_folder = "%s" % extensions.get("folder", "")
         if "meta_data" in current_explicit_attributes:
             del current_explicit_attributes["meta_data"]
 
@@ -229,6 +229,19 @@ def delete_host(module, base_url, headers):
         )
 
 
+def normalize_folder(folder):
+    if folder in ["", " ", "/", "//"]:
+        return "/"
+
+    if not folder.startswith("/"):
+        folder = "/%s" % folder
+
+    if folder.endswith("/"):
+        folder = folder.rstrip("/")
+
+    return folder
+
+
 def run_module():
     # define available arguments/parameters a user can pass to the module
     module_args = dict(
@@ -268,10 +281,7 @@ def run_module():
     state = module.params.get("state", "present")
 
     if "folder" in module.params:
-        if not module.params["folder"].startswith("/"):
-            module.params["folder"] = "/" + module.params["folder"]
-    else:
-        module.params["folder"] = "/"
+        module.params["folder"] = normalize_folder(module.params["folder"])
 
     # Determine the current state of this particular host
     (
@@ -286,10 +296,10 @@ def run_module():
         headers["If-Match"] = etag
         msg_tokens = []
 
-        if current_folder.endswith("/"):
-            current_folder = current_folder.rstrip("/")
+        current_folder = normalize_folder(current_folder)
 
         if current_folder != module.params["folder"]:
+            #exit_failed(module, "current: %s, new: %s" % (current_folder, module.params["folder"]))
             move_host(module, base_url, headers)
             msg_tokens.append("Host was moved.")
 
