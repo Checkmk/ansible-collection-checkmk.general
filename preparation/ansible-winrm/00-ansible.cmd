b114bcdac4279b9363cd5085794cea733fbaa7ab
muehlings <80385927+muehlings@users.noreply.github.com>
2022-10-14 18:16:18 +0200

    Add preparation files for Windows systems
    Using https://github.com/ansible/ansible/blob/devel/examples/scripts/ConfigureRemotingForAnsible.ps1

---
+++ preparation/ansible-winrm/00-ansible.cmd
@@ -0,0 +1,21 @@
+@echo off
+set CURRENT_PATH=%~dp0
+echo %CURRENT_PATH%
+
+
+rem https://stackoverflow.com/questions/4051883/batch-script-how-to-check-for-admin-rights
+goto check_Permissions
+
+:check_Permissions
+    echo Administrative permissions required. Detecting permissions...
+    
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Success: Administrative permissions confirmed. Setting up everything for Ansible...
+        %CURRENT_PATH%\01-winrm-quickconfig.cmd & %CURRENT_PATH%\02-ansible-winrm.cmd & %CURRENT_PATH%\03-allow-icmp.cmd
+pause
+    ) else (
+        echo Failure: Current permissions inadequate. You need to run this script as administrator!
+    )
+    
+    pause >nul
