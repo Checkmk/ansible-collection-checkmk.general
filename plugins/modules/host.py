176e42d12c5c81d9799c24677434d8ced9d88a4e
Mathias Buresch <mathias.buresch@de.clara.net>
2023-01-30 12:27:01 +0100

    fix format

--- plugins/modules/host.py
+++ plugins/modules/host.py
@@ -4,9 +4,6 @@
 # Copyright: (c) 2022, Robin Gierse <robin.gierse@tribe29.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import absolute_import, division, print_function
-from ansible.module_utils.urls import fetch_url
-from ansible.module_utils.basic import AnsibleModule
-import json
 
 __metaclass__ = type
 
@@ -158,6 +155,11 @@ message:
     sample: 'Host created.'
 """
 
+import json
+
+from ansible.module_utils.urls import fetch_url
+from ansible.module_utils.basic import AnsibleModule
+
 
 def exit_failed(module, msg):
     result = {"msg": msg, "changed": False, "failed": True}
@@ -186,8 +188,7 @@ def get_current_host_state(module, base_url, headers):
     parameters = "?effective_attributes=true"
     url = base_url + api_endpoint + parameters
 
-    response, info = fetch_url(
-        module, url, data=None, headers=headers, method="GET")
+    response, info = fetch_url(module, url, data=None, headers=headers, method="GET")
 
     if info["status"] == 200:
         body = json.loads(response.read())
@@ -286,8 +287,7 @@ def delete_host(module, base_url, headers):
     api_endpoint = "/objects/host_config/" + module.params.get("name")
     url = base_url + api_endpoint
 
-    response, info = fetch_url(
-        module, url, data=None, headers=headers, method="DELETE")
+    response, info = fetch_url(module, url, data=None, headers=headers, method="DELETE")
 
     if info["status"] != 204:
         exit_failed(
@@ -381,8 +381,7 @@ def run_module():
         ),
     )
 
-    module = AnsibleModule(argument_spec=module_args,
-                           supports_check_mode=False)
+    module = AnsibleModule(argument_spec=module_args, supports_check_mode=False)
 
     # Use the parameters to initialize some common variables
     headers = {
@@ -412,8 +411,7 @@ def run_module():
     # make sure if state is 'cluster' that nodes are set
     cluster_nodes = module.params.get("nodes", [])
     if state == "cluster" and len(cluster_nodes) == 0:
-        exit_failed(
-            module, "State 'cluster' is set. Defining 'nodes' is required.")
+        exit_failed(module, "State 'cluster' is set. Defining 'nodes' is required.")
 
     # Determine the current state of this particular host
     (
@@ -458,8 +456,7 @@ def run_module():
         if len(msg_tokens) >= 1:
             exit_changed(module, " ".join(msg_tokens))
         else:
-            exit_ok(
-                module, "Host already present. All explicit attributes as desired.")
+            exit_ok(module, "Host already present. All explicit attributes as desired.")
 
     elif state == "present" and current_state == "absent":
         create_host(module, attributes, base_url, headers)
