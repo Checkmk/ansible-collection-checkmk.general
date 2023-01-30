e75b85de69b2252821b5e74a0218d11ccce8410e
Mathias Buresch <mathias.buresch@de.clara.net>
2023-01-30 10:49:19 +0100

    fix formatting

--- plugins/modules/host.py
+++ plugins/modules/host.py
@@ -4,6 +4,9 @@
 # Copyright: (c) 2022, Robin Gierse <robin.gierse@tribe29.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import absolute_import, division, print_function
+from ansible.module_utils.urls import fetch_url
+from ansible.module_utils.basic import AnsibleModule
+import json
 
 __metaclass__ = type
 
@@ -155,11 +158,6 @@ message:
     sample: 'Host created.'
 """
 
-import json
-
-from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils.urls import fetch_url
-
 
 def exit_failed(module, msg):
     result = {"msg": msg, "changed": False, "failed": True}
@@ -188,7 +186,8 @@ def get_current_host_state(module, base_url, headers):
     parameters = "?effective_attributes=true"
     url = base_url + api_endpoint + parameters
 
-    response, info = fetch_url(module, url, data=None, headers=headers, method="GET")
+    response, info = fetch_url(
+        module, url, data=None, headers=headers, method="GET")
 
     if info["status"] == 200:
         body = json.loads(response.read())
@@ -287,7 +286,8 @@ def delete_host(module, base_url, headers):
     api_endpoint = "/objects/host_config/" + module.params.get("name")
     url = base_url + api_endpoint
 
-    response, info = fetch_url(module, url, data=None, headers=headers, method="DELETE")
+    response, info = fetch_url(
+        module, url, data=None, headers=headers, method="DELETE")
 
     if info["status"] != 204:
         exit_failed(
@@ -375,13 +375,14 @@ def run_module():
         ),
         attributes=dict(type="raw", default=[]),
         folder=dict(type="str", default="/"),
-        nodes=dict(type="list",elements="str", default=[]),
+        nodes=dict(type="list", elements="str", default=[]),
         state=dict(
             type="str", default="present", choices=["present", "cluster", "absent"]
         ),
     )
 
-    module = AnsibleModule(argument_spec=module_args, supports_check_mode=False)
+    module = AnsibleModule(argument_spec=module_args,
+                           supports_check_mode=False)
 
     # Use the parameters to initialize some common variables
     headers = {
@@ -411,7 +412,8 @@ def run_module():
     # make sure if state is 'cluster' that nodes are set
     cluster_nodes = module.params.get("nodes", [])
     if state == "cluster" and len(cluster_nodes) == 0:
-        exit_failed(module, "State 'cluster' is set. Defining 'nodes' is required.")
+        exit_failed(
+            module, "State 'cluster' is set. Defining 'nodes' is required.")
 
     # Determine the current state of this particular host
     (
@@ -424,7 +426,7 @@ def run_module():
     ) = get_current_host_state(module, base_url, headers)
 
     # Handle the host accordingly to above findings and desired state
-    if (state == "present" or state == "cluster") and (current_state == "present") :
+    if (state == "present" or state == "cluster") and (current_state == "present"):
         headers["If-Match"] = etag
         msg_tokens = []
 
@@ -448,21 +450,21 @@ def run_module():
         if attributes != {} and current_explicit_attributes != attributes:
             set_host_attributes(module, attributes, base_url, headers)
             msg_tokens.append("Host attributes changed.")
-            
+
         if state == "cluster" and current_cluster_nodes != cluster_nodes:
             update_cluster_nodes(module, base_url, headers)
             msg_tokens.append("Cluster nodes changed.")
 
-
         if len(msg_tokens) >= 1:
             exit_changed(module, " ".join(msg_tokens))
         else:
-            exit_ok(module, "Host already present. All explicit attributes as desired.")
+            exit_ok(
+                module, "Host already present. All explicit attributes as desired.")
 
     elif state == "present" and current_state == "absent":
         create_host(module, attributes, base_url, headers)
         exit_changed(module, "Host created.")
-        
+
     elif state == "cluster" and current_state == "absent":
         create_cluster_host(module, attributes, base_url, headers)
         exit_changed(module, "Cluster Host created.")
