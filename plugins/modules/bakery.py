0d50975b7abaefea89b79b06e043999d59bd4be0
Robin Gierse <robin.gierse@tribe29.com>
2023-07-28 12:27:22 +0200

    Trigger a run of the bakery module integration tests.

--- plugins/modules/bakery.py
+++ plugins/modules/bakery.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- encoding: utf-8; py-indent-offset: 4 -*-
 
+# Trigger
+
 # Copyright: (c) 2023, Max Sickora <max.sickora@checkmk.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import absolute_import, division, print_function
