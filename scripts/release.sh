adf95f1b8f92b1b01e7cd10dc79a99adb3968a2f
Robin Gierse <robin.gierse@tribe29.com>
2023-08-09 16:48:50 +0200

    Bump Checkmk version for tests.

--- scripts/release.sh
+++ scripts/release.sh
@@ -16,7 +16,7 @@ collection_dir="${script_dir%/*}"
 
 # Update these as necessary:
 checkmk_ancient="2.0.0p37"
-checkmk_oldstable="2.1.0p31"
+checkmk_oldstable="2.1.0p32"
 checkmk_stable="2.2.0p7"
 
 while getopts 's:t:' OPTION; do
