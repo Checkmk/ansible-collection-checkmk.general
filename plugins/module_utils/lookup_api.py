c892295e9faa6711dbcdf06db39a9c9c33a6997e
Lars Getwan <lars.getwan@checkmk.com>
2023-08-25 08:07:48 +0200

    sanity, again.

--- plugins/module_utils/lookup_api.py
+++ plugins/module_utils/lookup_api.py
@@ -8,11 +8,9 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
-# from urllib.parse import urlencode
-
+from ansible.module_utils.common.text.converters import to_text
 from ansible.module_utils.six.moves.urllib.parse import urlencode
-from ansible.module_utils.common.text.converters import to_native, to_text
-from ansible.module_utils.urls import ConnectionError, SSLValidationError, open_url
+from ansible.module_utils.urls import open_url
 
 
 class CheckMKLookupAPI:
