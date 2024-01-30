07a5b79b0bd48aa6577f22388cc298412b700b8c
Robin Gierse <robin.gierse@tribe29.com>
2024-01-30 17:41:08 -0500

    Bump Checkmk version.

--- scripts/release.sh
+++ scripts/release.sh
@@ -17,7 +17,7 @@ collection_dir="${script_dir%/*}"
 # Update these as necessary:
 checkmk_ancient="2.0.0p39"
 checkmk_oldstable="2.1.0p38"
-checkmk_stable="2.2.0p20"
+checkmk_stable="2.2.0p21"
 checkmk_beta="2.3.0-$(date +%Y.%m.%d)"
 
 while getopts 's:t:' OPTION; do
