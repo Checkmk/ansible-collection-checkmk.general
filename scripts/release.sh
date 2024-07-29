125aa2ae3460854a2e508ded922b627428220093
Robin Gierse <robin.gierse@checkmk.com>
2024-07-29 16:30:49 +0200

    Bump Checkmk version.

--- scripts/release.sh
+++ scripts/release.sh
@@ -17,7 +17,7 @@ collection_dir="${script_dir%/*}"
 # Update these as necessary:
 checkmk_ancient="2.1.0p46"
 checkmk_oldstable="2.2.0p31"
-checkmk_stable="2.3.0p10"
+checkmk_stable="2.3.0p11"
 
 while getopts 's:t:' OPTION; do
   case "$OPTION" in
