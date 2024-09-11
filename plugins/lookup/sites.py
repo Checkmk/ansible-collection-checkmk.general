245a56f38472c2ccaf7465c811c52a548d1dfd45
Lars Getwan <lars.getwan@checkmk.com>
2024-09-11 15:11:29 +0200

    Created Lookup Modules 'site' and 'sites'.

---
+++ plugins/lookup/sites.py
@@ -0,0 +1,153 @@
+# Copyright: (c) 2023, Lars Getwan <lars.getwan@checkmk.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+DOCUMENTATION = """
+    name: sites
+    author: Lars Getwan (@lgetwan)
+    version_added: "5.3.0"
+
+    short_description: Get a list of all sites
+
+    description:
+      - Returns a list of all sites and their configuration.
+
+    options:
+
+      server_url:
+        description: URL of the Checkmk server.
+        required: True
+        vars:
+          - name: ansible_lookup_checkmk_server_url
+        env:
+          - name: ANSIBLE_LOOKUP_CHECKMK_SERVER_URL
+        ini:
+          - section: checkmk_lookup
+            key: server_url
+
+      site:
+        description: Site name for REST API access.
+        required: True
+        vars:
+          - name: ansible_lookup_checkmk_site
+        env:
+          - name: ANSIBLE_LOOKUP_CHECKMK_SITE
+        ini:
+          - section: checkmk_lookup
+            key: site
+
+      automation_user:
+        description: Automation user for the REST API access.
+        required: True
+        vars:
+          - name: ansible_lookup_checkmk_automation_user
+        env:
+          - name: ANSIBLE_LOOKUP_CHECKMK_AUTOMATION_USER
+        ini:
+          - section: checkmk_lookup
+            key: automation_user
+
+      automation_secret:
+        description: Automation secret for the REST API access.
+        required: True
+        vars:
+          - name: ansible_lookup_checkmk_automation_secret
+        env:
+          - name: ANSIBLE_LOOKUP_CHECKMK_AUTOMATION_SECRET
+        ini:
+          - section: checkmk_lookup
+            key: automation_secret
+
+      validate_certs:
+        description: Whether or not to validate TLS certificates.
+        type: boolean
+        required: False
+        default: True
+        vars:
+          - name: ansible_lookup_checkmk_validate_certs
+        env:
+          - name: ANSIBLE_LOOKUP_CHECKMK_VALIDATE_CERTS
+        ini:
+          - section: checkmk_lookup
+            key: validate_certs
+
+    notes:
+      - Like all lookups, this runs on the Ansible controller and is unaffected by other keywords such as 'become'.
+        If you need to use different permissions, you must change the command or run Ansible as another user.
+      - Alternatively, you can use a shell/command task that runs against localhost and registers the result.
+      - The directory of the play is used as the current working directory.
+      - It is B(NOT) possible to assign other variables to the variables mentioned in the C(vars) section!
+        This is a limitation of Ansible itself.
+"""
+
+EXAMPLES = """
+- name: Get all sites defined in the environment.
+  ansible.builtin.debug:
+    msg: "Site: {{ item.extensions }}"
+  loop: "{{
+    lookup('checkmk.general.sites',
+        server_url=server_url,
+        site=site,
+        automation_user=automation_user,
+        automation_secret=automation_secret,
+        validate_certs=False
+        )
+    }}"
+  loop_control:
+    label: "{{ item.id }}"
+"""
+
+RETURN = """
+  _list:
+    description:
+      - A list of all sites
+    type: list
+    elements: str
+"""
+
+import json
+
+from ansible.errors import AnsibleError
+from ansible.plugins.lookup import LookupBase
+from ansible_collections.checkmk.general.plugins.module_utils.lookup_api import (
+    CheckMKLookupAPI,
+)
+
+
+class LookupModule(LookupBase):
+    def run(self, terms, variables, **kwargs):
+        regex_params = {}
+        self.set_options(var_options=variables, direct=kwargs)
+        server_url = self.get_option("server_url")
+        site = self.get_option("site")
+        user = self.get_option("automation_user")
+        secret = self.get_option("automation_secret")
+        validate_certs = self.get_option("validate_certs")
+
+        site_url = server_url + "/" + site
+
+        api = CheckMKLookupAPI(
+            site_url=site_url,
+            user=user,
+            secret=secret,
+            validate_certs=validate_certs,
+        )
+
+        response = json.loads(api.get("/domain-types/site_connection/collections/all"))
+
+        if "code" in response:
+            raise AnsibleError(
+                "Received error for %s - %s: %s"
+                % (
+                    response.get("url", ""),
+                    response.get("code", ""),
+                    response.get("msg", ""),
+                )
+            )
+
+        site_list = response.get("value")
+
+        return [site_list]
