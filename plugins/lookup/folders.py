a53b25d055a0c59bde6aaefa83188830a5180e9e
Robin Gierse <robin.gierse@checkmk.com>
2025-06-06 16:26:31 +0200

    Fix typos in module documentation.

--- plugins/lookup/folders.py
+++ plugins/lookup/folders.py
@@ -23,7 +23,7 @@ DOCUMENTATION = """
         required: True
 
       server_url:
-        description: URL of the Checkmk server
+        description: URL of the Checkmk server.
         required: True
         vars:
           - name: checkmk_var_server_url
