869b5a1a95cbf4a5916da3905122725626f96d4c
Robin Gierse <robin.gierse@checkmk.com>
2025-07-01 10:26:48 +0200

    Update Ansible Versions for testing.

--- scripts/release.sh
+++ scripts/release.sh
@@ -57,7 +57,7 @@ find "${collection_dir}/roles/" -type f -name all.yml -exec sed -i "s/2.2.0.*/${
 find "${collection_dir}/roles/" -type f \( -name "main.yml" -o -name "argument_specs.yml" \) -exec sed -i "s/2.4.0.*/${checkmk_stable}\"/g" {} \; && echo "Updated default Checkmk version for roles to ${checkmk_stable}."
 find "${collection_dir}/roles/" -type f -name README.md -exec sed -i "s/2.4.0.*/${checkmk_stable}\"/g" {} \; && echo "Updated default Checkmk version in roles README to ${checkmk_stable}."
 # Support Matrix
-grep "${target_version}" "${collection_dir}/SUPPORT.md" > /dev/null || echo "${target_version} | ${checkmk_ancient}, ${checkmk_oldstable}, ${checkmk_stable} | 2.15, 2.16, 2.17 | None" >> "${collection_dir}/SUPPORT.md" && echo "Added line to compatibility matrix in SUPPORT.md."
+grep "${target_version}" "${collection_dir}/SUPPORT.md" > /dev/null || echo "${target_version} | ${checkmk_ancient}, ${checkmk_oldstable}, ${checkmk_stable} | 2.16, 2.17, 2.18 | None" >> "${collection_dir}/SUPPORT.md" && echo "Added line to compatibility matrix in SUPPORT.md."
 # pyproject.toml
 sed -i "s/version = \"${source_version}\"/version = \"${target_version}\"/g" "${collection_dir}/pyproject.toml" && echo "Updated Checkmk version in pyproject.toml from ${source_version} to ${target_version}."
 echo "# End changes section."
