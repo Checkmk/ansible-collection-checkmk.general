406c130cac1d2c00d7628c71b46efe832e90b3b4
Lars Getwan <lars.getwan@checkmk.com>
2025-08-20 08:51:20 +0200

    Our internal CheckmkAPI now supports basic and cookie authentication.

--- plugins/module_utils/utils.py
+++ plugins/module_utils/utils.py
@@ -41,6 +41,17 @@ def base_argument_spec():
             no_log=True,
             fallback=(env_fallback, ["CHECKMK_VAR_AUTOMATION_SECRET"]),
         ),
+        automation_auth_type=dict(
+            type="str",
+            choices=["bearer", "basic", "cookie"],
+            default="bearer",
+            fallback=(env_fallback, ["CHECKMK_VAR_AUTH_TYPE"]),
+        ),
+        automation_auth_cookie=dict(
+            type="str",
+            no_log=True,
+            fallback=(env_fallback, ["CHECKMK_VAR_AUTH_COOKIE"]),
+        ),
     )
 
 
