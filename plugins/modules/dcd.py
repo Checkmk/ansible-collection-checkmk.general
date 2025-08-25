b91efc7f2bf8d3284853d05f68f15dcde1adb399
Lars Getwan <lars.getwan@checkmk.com>
2025-08-25 07:07:00 +0200

    DCD module: Enable the integration test

--- plugins/modules/dcd.py
+++ plugins/modules/dcd.py
@@ -486,6 +486,8 @@ def run_module():
             module, msg="Error managing the DCD configuration: %s" % e, logger=logger
         )
 
+    exit_module(module, result=result, logger=logger)
+
 
 def main():
     """
