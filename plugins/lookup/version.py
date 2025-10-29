9cfeaa61d38459be1b4f24bd87294b2032ce3904
Lars Getwan <lars.getwan@checkmk.com>
2025-10-29 09:06:23 +0100

    Changed the rest of the lookup modules to support Bearer and Cookie auth, as well.

--- plugins/lookup/version.py
+++ plugins/lookup/version.py
@@ -74,9 +74,9 @@ class LookupModule(LookupBase):
         server_url = self.get_option("server_url")
         site = self.get_option("site")
         api_auth_type = self.get_option("api_auth_type") or "bearer"
+        api_auth_cookie = self.get_option("api_auth_cookie")
         automation_user = self.get_option("automation_user")
         automation_secret = self.get_option("automation_secret")
-        api_auth_cookie = self.get_option("api_auth_cookie")
         validate_certs = self.get_option("validate_certs")
 
         site_url = server_url + "/" + site
@@ -84,9 +84,9 @@ class LookupModule(LookupBase):
         api = CheckMKLookupAPI(
             site_url=site_url,
             api_auth_type=api_auth_type,
+            api_auth_cookie=api_auth_cookie,
             automation_user=automation_user,
             automation_secret=automation_secret,
-            api_auth_cookie=api_auth_cookie,
             validate_certs=validate_certs,
         )
 
