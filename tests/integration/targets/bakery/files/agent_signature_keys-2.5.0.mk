3692c2020c80a9d6ecca605336b19629cd168697
Robin Gierse <robin.gierse@checkmk.com>
2025-12-03 13:25:06 +0100

    Add support for Checkmk 2.5.0 in integration tests.

---
+++ tests/integration/targets/bakery/files/agent_signature_keys-2.5.0.mk
@@ -0,0 +1,44 @@
+# Written by Checkmk store (actually Ansible)
+
+agent_signature_keys.update({'41bbbe85-c69e-46cb-9440-d50855a95d93': {'alias': 'ansible_collection',
+                                          'certificate': '-----BEGIN '
+                                                         'CERTIFICATE-----\n'
+                                                         'MIICfzCCAeigAwIBAgIUTK1d2nx0BgVKC3TZpxb6zMpBAW8wDQYJKoZIhvcNAQEN\n'
+                                                         'BQAwSzEbMBkGA1UEAwwSYW5zaWJsZV9jb2xsZWN0aW9uMRkwFwYDVQQKDBBDaGVj\n'
+                                                         'a21rIFNpdGUgcHJvMREwDwYDVQQLDAhjbWthZG1pbjAeFw0yNTEyMzAwOTQ0Mjda\n'
+                                                         'Fw0zNTEyMzAwOTQ0MjdaMEsxGzAZBgNVBAMMEmFuc2libGVfY29sbGVjdGlvbjEZ\n'
+                                                         'MBcGA1UECgwQQ2hlY2ttayBTaXRlIHBybzERMA8GA1UECwwIY21rYWRtaW4wgZ8w\n'
+                                                         'DQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMh2u4x+Gx04hxte7C1M179P/HwNa7jS\n'
+                                                         '+AzEmhQ+S0s+tE+H7E658A6ad41lGpgGENStpjVo0332Am57sAfwmxKoEfqm4I0s\n'
+                                                         'a5tZ7ILJe+6oATQzelc1EaBYoaMKDXdkULsyVWifHcOebEmtGzFhtbDjPeERqOCC\n'
+                                                         'rkWcP6kfvfu9AgMBAAGjYDBeMAwGA1UdEwEB/wQCMAAwHQYDVR0OBBYEFBQ6i3PN\n'
+                                                         'RUSl/GLxrLv0RQAhWUhBMB8GA1UdIwQYMBaAFBQ6i3PNRUSl/GLxrLv0RQAhWUhB\n'
+                                                         'MA4GA1UdDwEB/wQEAwIHgDANBgkqhkiG9w0BAQ0FAAOBgQApmaXDomYfZMhrwJIZ\n'
+                                                         'MA3B0yd/l6VfHGn4X3h70g8T3JZWTIsemrpWwHSyhlYUVeNN3ai9HtT6AlDStORW\n'
+                                                         's0b6LSE6NAbR3P2zT264+zzhgGOFKUpbIXYi6ZkIV5IWZL1PQApYgnrnkg6+GAUJ\n'
+                                                         'kbJFvoknOT7uWi1YXP2iAXWzJg==\n'
+                                                         '-----END '
+                                                         'CERTIFICATE-----\n',
+                                          'date': 1767087867.6674378,
+                                          'not_downloaded': True,
+                                          'owner': 'cmkadmin',
+                                          'private_key': '-----BEGIN ENCRYPTED '
+                                                         'PRIVATE KEY-----\n'
+                                                         'MIIC3TBXBgkqhkiG9w0BBQ0wSjApBgkqhkiG9w0BBQwwHAQI9K4xU6MESgQCAggA\n'
+                                                         'MAwGCCqGSIb3DQIJBQAwHQYJYIZIAWUDBAEqBBDnJXTq12Ud9xjAlhsE08JiBIIC\n'
+                                                         'gJBpQMn3Ro6iVFSKO9WgnbguAB5v1lX7GdB/opX6cgH5SjkOGEw0VZkRHaoIXqog\n'
+                                                         'zB9UTZHzzJO9dG92gpLn5yvUel8H8q9EAVqLwOcCpOast6KA+zNl74VTfq919GYX\n'
+                                                         '4maJ3JU898RFYGolJvmhwraxPu1/A808rVHsnZVMeAKcRg9VcI5NHAG9ekWg1cqg\n'
+                                                         'YS84gUX4LmtiOeyrCxsPH22nrhATX8h8FnCnxRNUr8HOJ8dGb++G42DVsSxig7xV\n'
+                                                         'uBJjbF1i4NWyYYrddmX7M3K/ZEflMGD2ysKlZ8/ERlNxend0TRrTxL3RWkLiStP3\n'
+                                                         'XMrqLSr88+tc5bI1XfBHTqreU888NWalh5b5WmubqIhR4VbJlg368LrWAlbBAwWB\n'
+                                                         '0p12LNr3rDgCoLLSw4FY2G8wfm2JUdeuuP0IE0fzvNMLvpZZRb4aroAg7j/yNS8h\n'
+                                                         '3wAwrZCAlnvsj9zbe6VR3flin2KnoVUYFRH3mic6PxH/yaQ1DBJeqvH6fJAbVXDk\n'
+                                                         'jsRkItmNAGKY+hWRpVor6k0L610jIbqWd2DXZh700ac9HDjEIQ29dDqPhnKhD9Zk\n'
+                                                         '2GPT+v7lHPe2o56liGFhqIDYNnojbYZ1kbznfWcqpuXa9OxWiSu+0TM0YzotYioS\n'
+                                                         'WPb+UxrN7T6rcif84DJW4WJ5nYDz38npGJ8dAPO9HJCETn/G3O3QV+lZYxf/GVqE\n'
+                                                         '049efEwI45zhJ9PAWZoJpr0aIOZe8MD6XhT1QgEShbc8NkCgLPixrD4L4EzdGlWJ\n'
+                                                         '3q14H6jcZm5PW87hg45Wmz+x0dLYiuzrtTVoD9TiTxCDNBQfCJHiKcPLR6Hk7TJg\n'
+                                                         'gSq4iOEDtNuYokF18id6yOM=\n'
+                                                         '-----END ENCRYPTED '
+                                                         'PRIVATE KEY-----\n'}})
