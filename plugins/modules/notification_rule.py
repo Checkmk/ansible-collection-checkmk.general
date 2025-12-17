11d838c16e0fae4030feb44f0b5c3119b25a0632
BRAINEZ Nicolas <nicolas.brainez@ext.ec.europa.eu>
2025-12-17 21:58:55 +0100

    Add documentation and msteams example

--- plugins/modules/notification_rule.py
+++ plugins/modules/notification_rule.py
@@ -37,6 +37,120 @@ options:
             - This should match the structure returned by the Checkmk API.
         required: false
         type: dict
+        suboptions:
+            rule_properties:
+                description: Properties of the notification rule.
+                type: dict
+            notification_method:
+                description: The notification method configuration including plugin and bulking settings.
+                type: dict
+            contact_selection:
+                description: Selection of contacts to notify.
+                type: dict
+                suboptions:
+                    all_contacts_of_the_notified_object:
+                        description: Notify all contacts of the object.
+                        type: dict
+                    all_users:
+                        description: Notify all users.
+                        type: dict
+                    all_users_with_an_email_address:
+                        description: Notify all users with an email address.
+                        type: dict
+                    the_following_users:
+                        description: Notify specific users.
+                        type: dict
+                    members_of_contact_groups:
+                        description: Notify members of contact groups.
+                        type: dict
+                    explicit_email_addresses:
+                        description: Notify explicit email addresses.
+                        type: dict
+                    restrict_by_custom_macros:
+                        description: Restrict by custom macros.
+                        type: dict
+                    restrict_by_contact_groups:
+                        description: Restrict by contact groups.
+                        type: dict
+            conditions:
+                description: Conditions for when the rule applies.
+                type: dict
+                suboptions:
+                    match_sites:
+                        description: Match specific sites.
+                        type: dict
+                    match_folder:
+                        description: Match specific folder.
+                        type: dict
+                    match_host_tags:
+                        description: Match host tags.
+                        type: dict
+                    match_host_labels:
+                        description: Match host labels.
+                        type: dict
+                    match_host_groups:
+                        description: Match host groups.
+                        type: dict
+                    match_hosts:
+                        description: Match specific hosts.
+                        type: dict
+                    match_exclude_hosts:
+                        description: Exclude specific hosts.
+                        type: dict
+                    match_service_labels:
+                        description: Match service labels.
+                        type: dict
+                    match_service_groups:
+                        description: Match service groups.
+                        type: dict
+                    match_exclude_service_groups:
+                        description: Exclude service groups.
+                        type: dict
+                    match_service_groups_regex:
+                        description: Match service groups by regex.
+                        type: dict
+                    match_exclude_service_groups_regex:
+                        description: Exclude service groups by regex.
+                        type: dict
+                    match_services:
+                        description: Match specific services by regex.
+                        type: dict
+                    match_exclude_services:
+                        description: Exclude specific services.
+                        type: dict
+                    match_check_types:
+                        description: Match check types.
+                        type: dict
+                    match_plugin_output:
+                        description: Match plugin output.
+                        type: dict
+                    match_contact_groups:
+                        description: Match contact groups.
+                        type: dict
+                    match_service_levels:
+                        description: Match service levels.
+                        type: dict
+                    match_only_during_time_period:
+                        description: Match only during time period.
+                        type: dict
+                    match_host_event_type:
+                        description: Match host event types.
+                        type: dict
+                    match_service_event_type:
+                        description: Match service event types.
+                        type: dict
+                    restrict_to_notification_numbers:
+                        description: Restrict to notification numbers.
+                        type: dict
+                    throttle_periodic_notifications:
+                        description: Throttle periodic notifications.
+                        type: dict
+                    match_notification_comment:
+                        description: Match notification comment.
+                        type: dict
+                    event_console_alerts:
+                        description: Event console alerts.
+                        type: dict
 
     state:
         description: The desired state of the notification rule.
@@ -200,6 +314,120 @@ EXAMPLES = r"""
             url_prefix_for_links_to_checkmk:
               state: "disabled"
             disable_ssl_cert_verification:
+              state: "disabled"
+            http_proxy:
+              state: "disabled"
+        notification_bulking:
+          state: "disabled"
+      contact_selection:
+        all_contacts_of_the_notified_object:
+          state: "enabled"
+        all_users:
+          state: "disabled"
+        all_users_with_an_email_address:
+          state: "disabled"
+        the_following_users:
+          state: "disabled"
+        members_of_contact_groups:
+          state: "disabled"
+        explicit_email_addresses:
+          state: "disabled"
+        restrict_by_custom_macros:
+          state: "disabled"
+        restrict_by_contact_groups:
+          state: "disabled"
+      conditions:
+        match_sites:
+          state: "disabled"
+        match_folder:
+          state: "disabled"
+        match_host_tags:
+          state: "disabled"
+        match_host_labels:
+          state: "disabled"
+        match_host_groups:
+          state: "disabled"
+        match_hosts:
+          state: "disabled"
+        match_exclude_hosts:
+          state: "disabled"
+        match_service_labels:
+          state: "disabled"
+        match_service_groups:
+          state: "disabled"
+        match_exclude_service_groups:
+          state: "disabled"
+        match_service_groups_regex:
+          state: "disabled"
+        match_exclude_service_groups_regex:
+          state: "disabled"
+        match_services:
+          state: "disabled"
+        match_exclude_services:
+          state: "disabled"
+        match_check_types:
+          state: "disabled"
+        match_plugin_output:
+          state: "disabled"
+        match_contact_groups:
+          state: "disabled"
+        match_service_levels:
+          state: "disabled"
+        match_only_during_time_period:
+          state: "disabled"
+        match_host_event_type:
+          state: "disabled"
+        match_service_event_type:
+          state: "disabled"
+        restrict_to_notification_numbers:
+          state: "disabled"
+        throttle_periodic_notifications:
+          state: "disabled"
+        match_notification_comment:
+          state: "disabled"
+        event_console_alerts:
+          state: "disabled"
+    state: "present"
+
+# Create a Microsoft Teams notification rule
+- name: "Create Microsoft Teams notification rule"
+  checkmk.general.notification_rule:
+    server_url: "http://myserver/"
+    site: "mysite"
+    automation_user: "myuser"
+    automation_secret: "mysecret"
+    rule_config:
+      rule_properties:
+        description: "Teams notifications for critical alerts"
+        comment: "Managed by Ansible"
+        documentation_url: ""
+        do_not_apply_this_rule:
+          state: "disabled"
+        allow_users_to_deactivate:
+          state: "enabled"
+      notification_method:
+        notify_plugin:
+          option: "create_notification_with_the_following_parameters"
+          plugin_params:
+            plugin_name: "msteams"
+            webhook_url:
+              option: "explicit"
+              url: "https://outlook.office.com/webhook/YOUR/WEBHOOK/URL"
+            host_title:
+              state: "disabled"
+            service_title:
+              state: "disabled"
+            host_summary:
+              state: "disabled"
+            service_summary:
+              state: "disabled"
+            host_details:
+              state: "disabled"
+            service_details:
+              state: "disabled"
+            affected_host_groups:
+              state: "disabled"
+            url_prefix_for_links_to_checkmk:
               state: "disabled"
             http_proxy:
               state: "disabled"
