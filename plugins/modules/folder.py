f6f7bac07bf5f4e3b06025feea1697a418ac7235
Robin Gierse <robin.gierse@checkmk.com>
2026-01-20 11:39:37 +0100

    Fix findings after 'black' update.

--- plugins/modules/folder.py
+++ plugins/modules/folder.py
@@ -218,7 +218,7 @@ class FolderAPI(CheckmkAPI):
 
         self.desired = {}
 
-        (self.desired["parent"], self.desired["name"]) = self._normalize_path(
+        self.desired["parent"], self.desired["name"] = self._normalize_path(
             self.params.get("path")
         )
 
@@ -303,7 +303,7 @@ class FolderAPI(CheckmkAPI):
 
             if merged_attributes != current_attributes:
                 try:
-                    (c_m, m_c) = recursive_diff(current_attributes, merged_attributes)
+                    c_m, m_c = recursive_diff(current_attributes, merged_attributes)
                     changes.append("update attributes: %s" % json.dumps(m_c))
                 except Exception as e:
                     changes.append("update attributes")
@@ -332,20 +332,20 @@ class FolderAPI(CheckmkAPI):
                         msg="ERROR: The parameter remove_attributes of dict type is not supported for the paramter extended_functionality: false!",
                     )
 
-                (tmp_remove, tmp_rest) = (current_attributes, {})
+                tmp_remove, tmp_rest = (current_attributes, {})
                 if current_attributes != tmp_remove_attributes:
                     try:
-                        (c_m, m_c) = recursive_diff(
+                        c_m, m_c = recursive_diff(
                             current_attributes, tmp_remove_attributes
                         )
 
                         if c_m:
                             # if nothing to remove
                             if current_attributes == c_m:
-                                (tmp_remove, tmp_rest) = ({}, current_attributes)
+                                tmp_remove, tmp_rest = ({}, current_attributes)
                             else:
-                                (c_c_m, c_m_c) = recursive_diff(current_attributes, c_m)
-                                (tmp_remove, tmp_rest) = (c_c_m, c_m)
+                                c_c_m, c_m_c = recursive_diff(current_attributes, c_m)
+                                tmp_remove, tmp_rest = (c_c_m, c_m)
                     except Exception as e:
                         self.module.fail_json(
                             msg="ERROR: incompatible parameter: remove_attributes!",
