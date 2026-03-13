27e268055d67a0a08fcfcc81de1e375978f98cee
Robin Gierse <robin.gierse@checkmk.com>
2026-03-13 07:31:41 +0100

    Fix workflows and release.sh.

--- scripts/release.sh
+++ scripts/release.sh
@@ -55,9 +55,9 @@ sed -i "s/version: ${source_version}/version: ${target_version}/g" "${collection
 ## Integration tests
 find "${collection_dir}/tests/integration/files/includes/vars/" -type f -name global.yml -exec sed -i "s/2.4.0.*/${checkmk_stable}\"/g" {} \; && echo "Updated Checkmk Stable version for integration tests includes to ${checkmk_stable}."
 ## GitHub Workflows
-find "${collection_dir}/.github/workflows/" -type f -name "ans-int-test-*.yaml" -exec sed -i "s/2.4.0.*/${checkmk_stable}\"/g" {} \; && echo "Updated Checkmk Stable version for GitHub Workflows to ${checkmk_stable}."
-find "${collection_dir}/.github/workflows/" -type f -name "ans-int-test-*.yaml" -exec sed -i "s/2.3.0.*/${checkmk_oldstable}\"/g" {} \; && echo "Updated Checkmk Stable version for GitHub Workflows to ${checkmk_oldstable}."
-find "${collection_dir}/.github/workflows/" -type f -name "ans-int-test-*.yaml" -exec sed -i "s/2.2.0.*/${checkmk_ancient}\"/g" {} \; && echo "Updated Checkmk Stable version for GitHub Workflows to ${checkmk_ancient}."
+find "${collection_dir}/.github/workflows/" -type f -name "ans-int-test-*.yaml" -exec sed -i "s/2.4.0.*/${checkmk_stable}/g" {} \; && echo "Updated Checkmk Stable version for GitHub Workflows to ${checkmk_stable}."
+find "${collection_dir}/.github/workflows/" -type f -name "ans-int-test-*.yaml" -exec sed -i "s/2.3.0.*/${checkmk_oldstable}/g" {} \; && echo "Updated Checkmk Stable version for GitHub Workflows to ${checkmk_oldstable}."
+find "${collection_dir}/.github/workflows/" -type f -name "ans-int-test-*.yaml" -exec sed -i "s/2.2.0.*/${checkmk_ancient}/g" {} \; && echo "Updated Checkmk Stable version for GitHub Workflows to ${checkmk_ancient}."
 ## Molecule tests
 find "${collection_dir}/roles/" -type f -name all.yml -exec sed -i "s/2.4.0.*/${checkmk_stable}\"/g" {} \; && echo "Updated Checkmk Stable version for molecule tests to ${checkmk_stable}."
 find "${collection_dir}/roles/" -type f -name all.yml -exec sed -i "s/2.3.0.*/${checkmk_oldstable}\"/g" {} \; && echo "Updated Checkmk Oldstable version for molecule tests to ${checkmk_oldstable}."
